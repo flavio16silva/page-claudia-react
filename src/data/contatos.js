4bc45ebb9484d761f851042e7b6ec436b2d4223d
Flávio Silva <fal_silva@hotmail.com>
2026-03-25 16:06:21 -0300

    Contatos.js -> deletado comentário e add novo color

--- src/data/contatos.js
+++ src/data/contatos.js
@@ -1,4 +1,3 @@
-// 📁 src/data/contatos.js
 import { FaWhatsapp, FaEnvelope } from 'react-icons/fa'
 
 export const contatos = [
@@ -6,14 +5,14 @@ export const contatos = [
     id: 1,
     icone: FaWhatsapp,
     href: "https://wa.me/5511999999999",
-    cor: "hover:text-green-400",
+    cor: "hover:text-red-400",
     label: "WhatsApp"
   },
   {
     id: 2,
     icone: FaEnvelope,
     href: "mailto:contato@draclaudia.com.br",
-    cor: "hover:text-green-400",
+    cor: "hover:text-red-400",
     label: "Email"
   }
 ]
