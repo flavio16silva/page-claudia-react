333fd928d65d2d237796ffd668d9aae800dc34bd
Flávio Silva <fal_silva@hotmail.com>
2026-03-30 11:49:13 -0300

    Data -> Arquivo de data: array empresas

---
+++ src/data/empresas.js
@@ -0,0 +1,30 @@
+export const empresas = [
+  {
+    id: 1,
+    title: "Setor Financeiro",
+    description: "Ajudar a gerenciar o estresse relacionado às demandas intensas do setor, melhorar a comunicação entre os membros da equipe, fornecer estratégias eficazes para lidar com a pressão e promover a inteligência emocional.",
+    improvements: "Gestão do Estresse e Resiliência, Comunicação Interpessoal, Inteligência Emocional, Motivação e Engajamento, Desempenho Individual e Coletivo, Descoberta de Habilidades.",
+    // image: "/equipe.png"
+  },
+  {
+    id: 2,
+    title: "Trabalho em Equipe",
+    description: "Compreender as interações, contribuindo para uma comunicação melhor, reduzir mal-entendidos e agilizar as decisões. Facilitar a colaboração e a delegação de tarefas. Ter um ambiente seguro estimula a inovação, a criatividade e a capacidade da equipe. ",
+    improvements: "Comunicação Eficaz, Gestão de Conflitos, Tomada de Decisões Rápidas, Desenvolvimento da Confiança, Adaptação a Mudanças, Estímulo à Inovação e Criatividade.",
+    // image: "/empresariais/trabEquipe1.jpg"
+  },
+  {
+    id: 3,
+    title: "Colaboradores",
+    description: "Desenvolver habilidades sociais, gerir os sentimentos, o bem-estar e fortalecimento da inteligência emocional. Orientar na carreira e realizar avaliações individuais, estimular o crescimento profissional e intervir na resolução de conflitos criam um ambiente inovador.",
+    improvements: "Apoio a produtividade, Ganho de Confiança, Inteligência Emocional, Orientação na Gestão de Carreira, Desenvolvimento de Respostas a Conflitos Interpessoais.",
+    // image: "/empresariais/colab1.jpg"
+  },
+  {
+    id: 4,
+    title: "Líderes",
+    description: "Treinamentos específicos em habilidades de liderança, contribuem para o avanço de competências como inteligência emocional, comunicação assertiva e resolução de conflitos. Permitir aos líderes terem um ambiente de trabalho colaborativo e eficiente.",
+    improvements: "Desenvolvimento de Interações, Tomada de decisões Equilibradas, Liderança Motivada, Tratativa de Conflitos, Autoconhecimento autêntico, Adaptação a Mudanças.",
+    // image: "/empresariais/lider1.jpg"
+  }
+]
