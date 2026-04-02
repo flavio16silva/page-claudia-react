1ab42ac5655c8a1f31d931c663c548c6a8224f8f
Flávio Silva <fal_silva@hotmail.com>
2026-04-01 21:19:59 -0300

    Data -> Contatos do footer alterado

--- src/data/contatos.js
+++ src/data/contatos.js
@@ -4,14 +4,14 @@ export const contatos = [
   {
     id: 1,
     icone: FaWhatsapp,
-    href: "https://wa.me/5511999999999",
+    href: "https://wa.me/5571988232921",
     cor: "hover:text-red-400",
     label: "WhatsApp"
   },
   {
     id: 2,
     icone: FaEnvelope,
-    href: "mailto:contato@draclaudia.com.br",
+    href: "mailto:claudiapalma35@gmail.com",
     cor: "hover:text-red-400",
     label: "Email"
   }
